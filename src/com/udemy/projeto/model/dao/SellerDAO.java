7387de04d061b43f32cf052f587a8981f8879795
devopMarkz <marcosdev2002@gmail.com>
2024-11-05 17:38:36 -0300

    Criando DAO da classe Seller para implementar lógica de acesso ao banco de dados

---
+++ src/com/udemy/projeto/model/dao/SellerDAO.java
@@ -0,0 +1,20 @@
+package com.udemy.projeto.model.dao;
+
+import com.udemy.projeto.model.entities.Department;
+import com.udemy.projeto.model.entities.Seller;
+
+import java.util.List;
+
+public interface SellerDAO {
+
+    void insert(Seller seller);
+
+    void update(Seller seller);
+
+    void deleteById(int id);
+
+    Seller findById(int id);
+
+    List<Seller> findAll();
+    
+}
