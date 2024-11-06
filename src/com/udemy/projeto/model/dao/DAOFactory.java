efaf1b4d74b4e377cbdf12a5162189f6df8a9e5a
devopMarkz <marcosdev2002@gmail.com>
2024-11-06 17:18:33 -0300

    Ajustando DAOFactory para retornar um SellerDAO com construtor preenchido

--- src/com/udemy/projeto/model/dao/DAOFactory.java
+++ src/com/udemy/projeto/model/dao/DAOFactory.java
@@ -1,11 +1,12 @@
 package com.udemy.projeto.model.dao;
 
 import com.udemy.projeto.model.dao.implementation.SellerDAOJDBC;
+import com.udemy.projeto.util.DB;
 
 public class DAOFactory {
 
     public static SellerDAO createSellerDAO() {
-        return new SellerDAOJDBC();
+        return new SellerDAOJDBC(DB.getConnection());
     }
 
 }
